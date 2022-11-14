d0ca83b7912873b97ea876183eb3b76c9e7822df
kevinchangjk <kevinchangjk@gmail.com>
2022-11-15 00:04:35 +0800

    make new constants

--- src/chirple-api/api.js
+++ src/chirple-api/api.js
@@ -4,23 +4,28 @@ const { sleep } = require("./utils.js");
 const MAX_COUNT = 10;
 const TOTAL_ROUNDS = 1;
 const ROUND_DELAY = 3000;
+const PROXY_URL = "https://hidden-ocean-65167.herokuapp.com/";
+const HTTP_PORT = "localhost:3000";
 
 async function postCall(keyword, startDate, endDate, nextToken) {
-  const request = await fetch("https://hidden-ocean-65167.herokuapp.com/http://www.hashscraper.com/api/twitter", {
-    method: "POST",
-    body: JSON.stringify({
-      api_key: API_KEY,
-      keyword,
-      start_date: startDate,
-      end_date: endDate,
-      max_count: MAX_COUNT,
-      next_token: nextToken,
-    }),
-    headers: {
-      "Content-type": "application/json; version=2; charset=UTF-8",
-      Origin: "localhost:8000",
-    },
-  });
+  const request = await fetch(
+    `${PROXY_URL}http://www.hashscraper.com/api/twitter`,
+    {
+      method: "POST",
+      body: JSON.stringify({
+        api_key: API_KEY,
+        keyword,
+        start_date: startDate,
+        end_date: endDate,
+        max_count: MAX_COUNT,
+        next_token: nextToken,
+      }),
+      headers: {
+        "Content-type": "application/json; version=2; charset=UTF-8",
+        Origin: HTTP_PORT,
+      },
+    }
+  );
 
   const result = await request.json();
   return result;
