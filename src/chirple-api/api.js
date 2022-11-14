3420b7940b65a543ff06b6aa7a81c4caa2e44522
kevinchangjk <kevinchangjk@gmail.com>
2022-11-14 16:28:21 +0800

    fix api

--- src/chirple-api/api.js
+++ src/chirple-api/api.js
@@ -1,23 +1,22 @@
 const fs = require("fs");
 
 const API_KEY_FILE_NAME = "API_KEY";
-const API_KEY = fs.readFileSync(API_KEY_FILE_NAME).toString().substring(32);
+const API_KEY = fs.readFileSync(API_KEY_FILE_NAME).toString().substring(0, 32);
 
 async function postCall(keyword) {
   const request = await fetch("http://www.hashscraper.com/api/twitter", {
     method: "POST",
-    body: {
+    body: JSON.stringify({
       api_key: API_KEY,
       keyword,
-    },
+    }),
     headers: {
       "Content-type": "application/json; version=2; charset=UTF-8",
     },
   });
 
-  const result = JSON.stringify(request.json());
+  const result = await request.json();
 
-  console.log(request);
   console.log(result);
 }
 
