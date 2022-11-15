1c6aaaff6f8de2988ece1fa6ce90760c1da1cbf2
Shawn Kok <shawnkok00@gmail.com>
2022-11-15 16:28:01 +0800

    Update to Dashboard

--- src/web-app/App.js
+++ src/web-app/App.js
@@ -2,16 +2,31 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import HeroBanner from '../components/HeroBanner';
 import DashBoard from '../components/Dashboard/Dashboard';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { blue, purple } from '@mui/material/colors';
 import './App.css';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: blue[500]
+    },
+    secondary: {
+      main: purple[500]
+    }
+  }
+});
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <HeroBanner/>
-      <DashBoard/>
-      <Footer />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Header/>
+        <HeroBanner/>
+        <DashBoard/>
+        <Footer />
+      </div>
+    </ThemeProvider>
   );
 }
 
