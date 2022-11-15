8960f01e6349abaa30d566aab84e1040e61e50c9
Shawn Kok <shawnkok00@gmail.com>
2022-11-15 23:54:43 +0800

    Improved headerbar aesthetics

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Toolbar, Typography, Box } from "@mui/material";
 import logo from '../assets/ChirpleLogo.PNG';
 import React from "react";
 
@@ -8,20 +8,34 @@ export default function Header() {
     };
 
     const chirpleLogo = (
-        <><img src={logo} width="50" alt="logo" />
-        <Typography variant="h5" component="h1" sx={{
-            fontFamily: "Work Sans, sans-serif",
-            fontWeight: 600,
-            color: "#FFFEFE",
-            textAlign: "left"
-        }}>
-            Chirple
-        </Typography></>
+        <Box sx={{ flexDirection: "row", display: "flex", alignItems: "center", justifyContent: "space-between", ml: 4.5 }} width={1350} >
+            
+            <Typography variant="h5" component="h1" sx={{
+                fontFamily: "Work Sans, sans-serif",
+                fontWeight: 600,
+                fontSize: 33,
+                color: "#FFFEFE",
+                textAlign: "left", flexDirection: "row", display: "flex", alignItems: "center"
+            }}>
+                <img src={logo} width="70" alt="logo" />
+                Chirple
+            </Typography>
+            <Typography variant="h5" component="h1" sx={{
+                fontFamily: "Work Sans, sans-serif",
+                fontWeight: 200,
+                fontSize: 20,
+                color: "#FFFEFE",
+                textAlign: "left",
+                fontStyle: 'italic'
+            }}>
+                Boost your business with twitter analytics
+            </Typography>
+        </Box> 
     );
     
     return (
         <header>
-            <AppBar sx={{ backgroundColor: "#400CCC" }}>
+            <AppBar sx={{ backgroundColor: "#400CCC", height: 110, flexDirection: "row", display: "flex", alignItems: "center" }}>
                 {displayDesktop()}
             </AppBar>
         </header>
